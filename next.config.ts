d6c107ac5bc97652ebf1e27c6e3acae58c5c259e
AjiNurAji <ajinuraji090306@gmail.com>
2026-03-17 14:45:34 +0700

    feat: implement sidebar component with mobile support and add homepage content

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  // add allowed image domains
+  images: {
+    domains: ["github.com"],
+  }
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import Image from "next/image";
-import { Button } from "~/components/ui/button";
+import { ClientSidebar } from "~/components/elements/client-sidebar";
+import { SidebarProvider} from "~/components/ui/sidebar";
+import Homepage from "~/features/pages/homepage";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>Hello World</h1>
-        <Button>This is Button</Button>
-      </main>
-    </div>
-  );
+	return (
+		<div className="font-sans">
+			<SidebarProvider>
+				<ClientSidebar />
+				<main className="bg-red-100 min-h-screen w-full p-4">
+          <Homepage />
+				</main>
+			</SidebarProvider>
+		</div>
+	);
 }
