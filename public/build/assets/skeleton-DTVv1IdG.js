19c7e1b8b5d11dd368dd14ade3765a8309945c3c
AjiNurAji <ajinuraji090306@gmail.com>
2026-06-25 15:40:46 +0700

    build: compile frontend for certificate popup

---
+++ public/build/assets/skeleton-DTVv1IdG.js
@@ -0,0 +1,1 @@
+import{$ as e}from"./app-CRSt3-hY.js";import{f as t}from"./dist-BJO2nQa6.js";var n=e();function r({className:e,...r}){return(0,n.jsx)(`div`,{"data-slot":`skeleton`,className:t(`animate-pulse rounded-md bg-muted`,e),...r})}export{r as t};
