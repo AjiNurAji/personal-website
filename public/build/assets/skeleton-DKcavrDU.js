abbb172ba6696c6fcd91e41f83cf5f6b0fed5e90
AjiNurAji <ajinuraji090306@gmail.com>
2026-07-24 19:50:26 +0700

    fix: close div tag in projects section

---
+++ public/build/assets/skeleton-DKcavrDU.js
@@ -0,0 +1,1 @@
+import{at as e}from"./app-owFu4Bx1.js";import{f as t}from"./dist-Bcr-7DuV.js";var n=e();function r({className:e,...r}){return(0,n.jsx)(`div`,{"data-slot":`skeleton`,className:t(`animate-pulse rounded-md bg-muted`,e),...r})}export{r as t};
