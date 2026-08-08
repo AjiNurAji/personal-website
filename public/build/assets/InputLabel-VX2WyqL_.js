8cfadca16c36ed0a99c412d6402cb067cc6a75fd
AjiNurAji <ajinuraji090306@gmail.com>
2026-08-08 09:25:58 +0700

    fix: add padding bottom

--- public/build/assets/InputLabel-VX2WyqL_.js
+++
@@ -1,1 +0,0 @@
-import{st as e}from"./app-DoNKyBjZ.js";import{o as t}from"./field-BWeSIANR.js";var n=e();function r({value:e,children:r,...i}){return(0,n.jsx)(t,{...i,children:e||r})}export{r as t};
